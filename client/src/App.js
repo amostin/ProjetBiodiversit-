d6a28af42c4d3d0a32d920a89aab8b2a6c1517ee
cmiesse <c.miesse@students.ephec.be>
2020-03-07 10:45:36 +0100

    mise en classe de App

--- client/src/App.js
+++ client/src/App.js
@@ -1,46 +1,39 @@
 import React from "react";
 import "./App.css";
-import Points from "./components/Points/Points";
-import Admin from "./components/Admin/Admin";
-import Login from "./components/Login/Login";
-import Itineraire from "./components/Itineraire/Itineraire";
 import Categorie from "./components/Categorie/Categorie";
-import PointDetail from "./components/PointDetails/PointDetail";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Map from "./components/Map/Map";
+import Login from "./components/Login/Login";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import login from "./login.png";
+import PointDetail from "./components/PointDetails/PointDetail";
+import Itineraire from "./components/Itineraire/Itineraire";
 
-function App() {
-  return (
-    <Router>
-      <div className="App">
-        <Switch>
-          <Route path="/" exact component={Home}></Route>
-          <Route path="/login" component={Login}></Route>
-          <Route path="/admin" component={Admin}></Route>
-          <Route path="/pointInteret" exact component={Points} />
-          <Route path="/pointInteret/:idPoint" component={PointDetail} />
-          <Route path="/itineraire" component={Itineraire} />
-          <Route path="/categorie" component={Categorie} />
-        </Switch>
-      </div>
-    </Router>
-  );
+export default class App extends React.Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <Switch>
+            <Route path="/login" component={Login}></Route>
+            <Route
+              path="/pointInteret/:idPoint"
+              component={PointDetail}
+            ></Route>
+            <Route path="/itineraire" component={Itineraire}></Route>
+          </Switch>
+          <div className="right">
+            <Link to="login">
+              <img id="login" src={login} alt="" title="Connexion" />
+            </Link>
+          </div>
+
+          <div className="center">
+            <Categorie />
+            <br />
+            <Map />
+          </div>
+        </div>
+      </Router>
+    );
+  }
 }
-
-const Home = () => (
-  <div>
-    <div className="right">
-      <Link to="/login">
-        <img id="login" src={login} alt="" title="Connexion" />
-      </Link>
-    </div>
-
-    <div className="center">
-      <Categorie />
-      <br />
-      <Map />
-    </div>
-  </div>
-);
-export default App;
