48d828ca9be0533889ae5af727ab83c8f9a26a8b
Cyril <cyril.wastchenko@gmail.com>
2020-03-12 11:20:07 +0100

    Update CSS page admin
    Ajout tableau Modifier Point d'intérêt

--- client/src/components/Admin/Admin.js
+++ client/src/components/Admin/Admin.js
@@ -111,7 +111,8 @@ class Admin extends Component {
           <tbody>{places.map(this.renderPlaces)}</tbody>
         </table>
         <br />
-        <div id="addEntry">
+        <div id="addUpdateEntry">
+        <h3>Ajouter un point d'intérêt</h3>
           <label htmlFor="nom">Nom</label>
           <input
             name="nom"
@@ -231,7 +232,133 @@ class Admin extends Component {
             <option value="1">Oui</option>
           </select>
           <br />
-          <button id='ajouterPoint' onClick={this.addPoint}>Ajouter point</button>
+          <button id='ajouterPoint' onClick={this.addPoint}>Ajouter un point d'intérêt</button>
+
+
+
+        <h3>Modifier point d'intérêt</h3>
+        <div id="FormModifyEntry">
+          <label htmlFor="nom">Nom</label>
+          <input
+            name="nom"
+            id="nom"
+            type="text"
+            value={point.nom}
+            onChange={e =>
+              this.setState({ point: { ...point, nom: e.target.value } })
+            }
+          />
+          <br />
+          <label htmlFor="nomLatin">Nom latin</label>
+          <input
+            name="nomLatin"
+            id="nomLatin"
+            type="text"
+            value={point.nomLatin}
+            onChange={e =>
+              this.setState({ point: { ...point, nomLatin: e.target.value } })
+            }
+          />
+          <br />
+          <label htmlFor="adresse">Adresse</label>
+          <input
+            name="adresse"
+            id="adresse"
+            type="text"
+            value={point.adresse}
+            onChange={e =>
+              this.setState({
+                point: { ...point, adresse: e.target.value }
+              })
+            }
+          />
+          <br />
+          <label htmlFor="longitude">Longitude</label>
+          <input
+            name="longitude"
+            id="longitude"
+            type="number"
+            step="0.1"
+            value={point.longitude}
+            onChange={e =>
+              this.setState({
+                point: { ...point, longitude: e.target.value }
+              })
+            }
+          />
+          <br />
+          <label htmlFor="latitude">Latitude</label>
+          <input
+            name="latitude"
+            id="latitude"
+            type="number"
+            step="0.1"
+            value={point.latitude}
+            onChange={e =>
+              this.setState({
+                point: { ...point, latitude: e.target.value }
+              })
+            }
+          />
+          <br />
+          <label htmlFor="categorie" className="Right">Catégorie</label>
+          <select
+            name="categorie"
+            id="categorie"
+            type="text"
+            value={point.categorie}
+            onChange={e =>
+              this.setState({ point: { ...point, categorie: e.target.value } })
+            }
+          >
+            <option value="nature">Nature</option>
+            <option value="animaux">Animaux</option>
+            <option value="batiment">Batiment</option>
+          </select>
+          <br />
+          <label htmlFor="debut">Début</label>
+          <input
+            name="debut"
+            id="debut"
+            type="text"
+            value={point.debut}
+            onChange={e =>
+              this.setState({
+                point: { ...point, debut: e.target.value }
+              })
+            }
+          />
+          <br />
+          <label htmlFor="fin">Fin</label>
+          <input
+            name="fin"
+            id="fin"
+            type="text"
+            value={point.fin}
+            onChange={e =>
+              this.setState({
+                point: { ...point, fin: e.target.value }
+              })
+            }
+          />
+          <br />
+          <label htmlFor="accessibilite">Accessible</label>
+          <select
+            name="accessibilite"
+            id="accessibilite"
+            value={point.accessibilite}
+            onChange={e =>
+              this.setState({
+                point: { ...point, accessibilite: e.target.value }
+              })
+            }
+          >
+            <option value="0">Non</option>
+            <option value="1">Oui</option>
+          </select>
+          <br />
+          <button id='modifyEntry' onClick={this.updatePoint}>Modifier un point d'intérêt</button>
+        </div>
         </div>
       </div>
     );
